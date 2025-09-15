fa530f51a9270e0794a71f093bc20742c2414769
Kang-Jiho79 <giho2630@gmail.com>
2025-09-15 20:42:33 +0900

    main

--- character_grass.py
+++ character_grass.py
@@ -1,7 +1,56 @@
 from pico2d import *
+import math
 
 open_canvas()
 
-# fill here
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+x = 400
+y = 90
+while x < 780:
+    clear_canvas()
+    grass.draw(400, 30)
+    character.draw(x, y)
+    x += 2
+    delay(0.01)
+    update_canvas()
+while y < 560:
+    clear_canvas()
+    grass.draw(400, 30)
+    character.draw(x, y)
+    y += 2
+    delay(0.01)
+    update_canvas()
+while x > 10:
+    clear_canvas()
+    grass.draw(400, 30)
+    character.draw(x, y)
+    x -= 2
+    delay(0.01)
+    update_canvas()
+while y > 90:
+    clear_canvas()
+    grass.draw(400, 30)
+    character.draw(x, y)
+    y -= 2
+    delay(0.01)
+    update_canvas()
+while x <400:
+    clear_canvas()
+    grass.draw(400, 30)
+    character.draw(x, y)
+    x += 2
+    delay(0.01)
+    update_canvas()
+
+for i in range(-90, -90 - 360, -1):
+    clear_canvas()
+    grass.draw(400, 30)
+    character.draw(x, y)
+    x=400 + 240 * math.cos(math.radians(i))
+    y=330 + 240 * math.sin(math.radians(i))
+    delay(0.01)
+    update_canvas()
 
 close_canvas()
